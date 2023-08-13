f0e3cb8ea6bde3b3186c79bc1ea227f6504c2b8b
Edison Taborda <edisontabo@gmail.com>
2023-08-13 07:21:17 -0500

    update routes

--- src/Products.js
+++ src/Products.js
@@ -82,7 +82,7 @@ const Products = () => {
                   <Typography color="gray">{item.description}</Typography>
                 </CardBody>
                 <CardFooter className="pt-0">
-                  <Link to={`/product/${item.id}`}
+                  <Link to={`/reactjs-shopping/product/${item.id}`}
                     className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100">
                     <Button ripple={false} fullWidth={true} >Ver</Button>
                   </Link>
